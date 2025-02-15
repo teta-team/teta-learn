fc6f3216edcfb3e786f8057b239a83329ff637c0
AHSharifi <mr.amirhossein88@gmail.com>
2025-02-15 19:26:56 +0330

    update (css, Physics.js)

--- src/pages/lessons/Physics.js
+++ src/pages/lessons/Physics.js
@@ -7,31 +7,36 @@ function Physics() {
   const [projects, setProjects] = useState([]);
 
   async function loadProjects() {
-    let { data: projects, error } = await supabase.from("projects").select("*");
+    let { data: projects } = await supabase
+      .from("projects")
+      .select("*")
+      .eq("lesson", "chemistry");
     setProjects(projects);
   }
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    loadProjects();
   }, []);
   return (
-    <div className="physics lesson">
+    <div className="lesson">
       <div className="container">
         <div className="apps-title">
           <h2>برنامه های کاربردی</h2>
           <div className="under-line"></div>
         </div>
         <div className="d-flex row f-wrap">
-          {projects.map((project, index) => {
+          {projects.map((project, index) => (
             <Project
+              key={index}
               id={project.id}
               lesson={project.lesson}
               image={project.image_url}
               fname={project.name}
               text={project.text}
               tags={project.tags}
-            />;
-          })}
+            />
+          ))}
         </div>
         <div className="lesson-title">
           <h2>جوزه های فیزیک</h2>
