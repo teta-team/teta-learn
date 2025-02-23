c1f53a5191b6ef9a77e80b5494752f5c2364237d
AHSharifi <mr.amirhossein88@gmail.com>
2025-02-23 15:46:19 +0330

    finish contact sql

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,50 @@
+import { useEffect, useRef } from "react";
+import supabase from "../supabase";
+
 function Contact() {
+  const nameRef = useRef(null);
+  const phoneRef = useRef(null);
+  const emailRef = useRef(null);
+  const lessonRef = useRef(null);
+  const textRef = useRef(null);
+  const userName = nameRef.current.value;
+  const userPhone = phoneRef.current.value;
+  const userEmail = emailRef.current.value;
+  const userLesson = lessonRef.current.value;
+  const userText = textRef.current.value;
+
+  async function handleClick() {
+    if (
+      userName === "" &&
+      userPhone === "" &&
+      userEmail === "" &&
+      userText === ""
+    ) {
+      alert("لطفا مقادیر لازم را پر کنید!");
+    } else {
+      const { data, error } = await supabase
+        .from("requests")
+        .insert([
+          {
+            name: userName,
+            phone: userPhone,
+            email: userEmail,
+            lesson: userLesson,
+            text: userText,
+          },
+        ])
+        .select();
+
+      nameRef.current.value = "";
+      phoneRef.current.value = "";
+      emailRef.current.value = "";
+      lessonRef.current.value = "ریاضی";
+      textRef.current.value = "";
+
+      alert("درخواست شما ارسال شد");
+    }
+  }
+
   return (
     <div className="contact">
       <div className="container">
@@ -15,15 +61,30 @@ function Contact() {
             <div className="right">
               <div className="inputs">
                 <label>
-                  <input type="text" placeholder="نام و نام خانوادگی"></input>
+                  <input
+                    type="text"
+                    placeholder="نام و نام خانوادگی"
+                    ref={nameRef}
+                    required
+                  ></input>
                 </label>
                 <label>
-                  <input type="tel" placeholder="شماره تلفن"></input>
+                  <input
+                    type="tel"
+                    placeholder="شماره تلفن"
+                    ref={phoneRef}
+                    required
+                  ></input>
                 </label>
                 <label>
-                  <input type="email" placeholder="ایمیل"></input>
+                  <input
+                    type="email"
+                    placeholder="ایمیل"
+                    ref={emailRef}
+                    required
+                  ></input>
                 </label>
-                <select>
+                <select ref={lessonRef}>
                   <option>ریاضی</option>
                   <option>هندسه</option>
                   <option>شیمی</option>
@@ -31,8 +92,14 @@ function Contact() {
                   <option>سایر</option>
                 </select>
               </div>
-              <textarea placeholder="توضیحات را بنویسید"></textarea>
-              <button type="submit">ارسال</button>
+              <textarea
+                placeholder="توضیحات را بنویسید"
+                ref={textRef}
+                required
+              ></textarea>
+              <button type="submit" onClick={handleClick}>
+                ارسال
+              </button>
             </div>
             <div className="left">
               <img
