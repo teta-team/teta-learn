ad4d6f01641167616845a1c1bcc984122e8e3dce
AHSharifi <mr.amirhossein88@gmail.com>
2025-07-05 23:36:22 +0330

    responsive menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,6 +3,12 @@ import { Link } from "react-router-dom";
 
 function Menu() {
   const [darkMode, setDarkMode] = useState("light-mode");
+  const [open, setOpen] = useState(false);
+
+  // تابع برای بستن منو هنگام کلیک روی لینک
+  const closeMenu = () => {
+    setOpen(false);
+  };
 
   function changeTheme() {
     if (darkMode === "light-mode") {
@@ -36,13 +42,55 @@ function Menu() {
 
   return (
     <div className="d-flex row justify-content-between align-items-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="25"
+        height="25"
+        fill="currentColor"
+        className="bi bi-list menu-list-icon"
+        viewBox="0 0 16 16"
+        onClick={() => setOpen(!open)} // ساده‌سازی toggle
+      >
+        <path
+          fillRule="evenodd"
+          d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+        />
+      </svg>
       <Link to="/">تتالرن</Link>
-      <nav className="d-flex row">
-        <Link to="/">خانه</Link>
-        <Link to="/lessons">دروس</Link>
-        <Link to="/questions">سوالات</Link>
-        <Link to="/chat">پرسش و پاسخ</Link>
-        <Link to="/contact">ارتباط با ما</Link>
+      <nav className="d-flex row desktop-nav">
+        <Link to="/" onClick={closeMenu}>
+          خانه
+        </Link>
+        <Link to="/lessons" onClick={closeMenu}>
+          دروس
+        </Link>
+        <Link to="/questions" onClick={closeMenu}>
+          سوالات
+        </Link>
+        <Link to="/chat" onClick={closeMenu}>
+          پرسش و پاسخ
+        </Link>
+        <Link to="/contact" onClick={closeMenu}>
+          ارتباط با ما
+        </Link>
+        <div className="line"></div>
+      </nav>
+      <nav className={`d-flex row mobile-nav ${open ? "active" : ""}`}>
+        <Link to="/" onClick={closeMenu}>
+          خانه
+        </Link>
+        <Link to="/lessons" onClick={closeMenu}>
+          دروس
+        </Link>
+        <Link to="/questions" onClick={closeMenu}>
+          سوالات
+        </Link>
+        <Link to="/chat" onClick={closeMenu}>
+          پرسش و پاسخ
+        </Link>
+        <Link to="/contact" onClick={closeMenu}>
+          ارتباط با ما
+        </Link>
         <div className="line"></div>
       </nav>
       <button type="button" onClick={changeTheme}>
